ea6777427915b923fafb3a1b1899efc4eb4b115c
Jesiel Viana Pfeifer <JESIELPFEIFER@edu.unisinos.br>
2019-05-13 19:21:34 -0300

    Codigo feito na monitoria

--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 
 class ImpostoDeRenda {
+
 private:
     string nomeDoContribuinte, profissao, escolaridade;
     float rendaMensal, aliquota;
     int numeroDependentes, anoNascimento;
+
 public:
     ImpostoDeRenda();
 
@@ -30,7 +32,9 @@ public:
 
     float valorIr(float aliquota);
 
-    void cadastro(string nomeDoContribuint, string profissao, string escolaridade, float rendaMensal,
+    void imprimir(string nomeDoContribuinte, string profissao, string escolaridade, int idade);
+
+    void cadastro(string nomeDoContribuinte, string profissao, string escolaridade, float rendaMensal,
                   float aliquota, int numeroDepedentes, int anoNascimento);
 };
 
@@ -45,8 +49,9 @@ ImpostoDeRenda::ImpostoDeRenda() {
     anoNascimento = 0;
 }
 
-ImpostoDeRenda::ImpostoDeRenda(string nomeDoContribuinte_, string profissao_, string escolaridade_, float rendaMensal_,
-                               float aliquota_, int numeroDependentes_, int anoNascimento_) {
+/*ImpostoDeRenda::ImpostoDeRenda(string nomeDoContribuinte_, string profissao_, string escolaridade_, float rendaMensal_,
+                               float aliquota_, int numeroDependentes_, int anoNascimento_)
+{
     nomeDoContribuinte = nomeDoContribuinte_;
     profissao = profissao_;
     escolaridade = escolaridade_;
@@ -54,7 +59,7 @@ ImpostoDeRenda::ImpostoDeRenda(string nomeDoContribuinte_, string profissao_, st
     aliquota = aliquota_;
     numeroDependentes = numeroDependentes_;
     anoNascimento = anoNascimento_;
-}
+}*/
 
 ImpostoDeRenda::~ImpostoDeRenda() {
 }
@@ -72,13 +77,64 @@ void ImpostoDeRenda::cadastro(string nomeDoContribuinte_, string profissao_, str
 
 int ImpostoDeRenda::idade(int anoNascimento) {
     if (anoNascimento <= 2019) {
-        int idade = anoNascimento - 2019;
+        int idade = 2019 - anoNascimento ;
+        cout << "Idade eh: " << idade << endl;
         return idade;
     }
 }
 
+float rendaAnual(float rendaMensal) {
+    int rendaAnual = rendaMensal * 12;
+    return rendaAnual;
+}
+
+float rendaPerCapitaMensal(float rendaMensal, int numeroDependentes) {
+}
+
+float aliquotaIrMax(float aliquota) {
+}
+
+float aliquotaIrEf (float aliquota) {
+}
+
+float valoIr(float aliquota) {
+}
+
+void cadastro(string nomeDoContribuinte, string profissao, string escolaridade, float rendaMensal,
+              float aliquota, int numeroDepedentes, int anoNascimento) {
+}
+
+void ImpostoDeRenda::imprimir(string nomeDoContribuinte, string profissao, string escolaridade, int idade) {
+    cout << "Nome: " << nomeDoContribuinte << endl;
+    cout << "Ano de nascimento: " << idade << endl;
+    cout << "Profissao: " << profissao << endl;
+    cout << "Escolaridade: " << escolaridade << endl;
+}
+
 int main() {
-    ImpostoDeRenda();
+    string _nome, _escolaridade, _profissao;
+    int _ano;
+
+    ImpostoDeRenda Dados; //objeto
+
     cout << "Digite o nome do contribuinte: ";
+    cin >> _nome;
+    cout << endl;
+
+    cout << "Digite o ano de nascimento: ";
+    cin >> _ano;
+    cout << endl;
+
+    cout << "Digite sua escolaridade: " ;
+    cin >> _escolaridade;
+    cout << endl;
+
+    cout << "Digite sua profissao: " ;
+    cin >> _profissao;
+    cout << endl;
+
+    Dados.imprimir(_nome, _escolaridade, _profissao, _ano);
+    cout << endl;
+
     return 0;
 }
